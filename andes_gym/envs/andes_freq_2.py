45e7c563369472521f9e36a5e7f6d85f219682b8
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-04 23:41:55 -0400

    Update andes_freq_2.py

--- andes_gym/envs/andes_freq_2.py
+++ andes_gym/envs/andes_freq_2.py
@@ -67,7 +67,7 @@ class AndesPrimaryFreqControl(gym.Env):
         self.N_Gov = 5  # number of TG1 models
         self.N_Bus = 5  # let it be the number of generators for now
 
-        self.action_space = spaces.Box(low=0, high=0.3, shape=(self.N_Gov,))
+        self.action_space = spaces.Box(low=0, high=0, shape=(self.N_Gov,))
         self.observation_space = spaces.Box(low=-5, high=5, shape=(self.N_Gov,))
 
         self.i = 0  # index of the current action
