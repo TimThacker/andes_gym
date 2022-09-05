9effabde5ec5b8eb95e3c4c570a5ab75930669ee
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-05 11:13:11 -0400

    Add files via upload

---
+++ examples/train_prim_freq_ddpg.py
@@ -0,0 +1,48 @@
+import gym
+import pandas as pd
+import numpy as np
+import andes_gym
+import os
+import matplotlib.pyplot as plt
+import time
+import torch
+from stable_baselines3.ddpg.policies import MlpPolicy
+from stable_baselines3 import DDPG
+plot_episode = True
+save_dir = "delay_learning_200_action_40_GOV/"
+env = gym.make('AndesPrimaryFreqControl-v0')
+policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64])  # kwargs == keyword arguments
+model = DDPG(MlpPolicy, env, verbose=0, policy_kwargs=policy_kwargs, learning_starts=200)
+time_start = time.time()
+model.learn(total_timesteps=10000)  # we need to change the total steps with action numbers
+print("training {} completed using {}".format(id, time.time() - time_start))
+obs = env.reset()
+done = False
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    if done is True:
+        break
+plt.rcParams.update({'font.family': 'Arial'})
+plt.figure(figsize=(9, 7))
+plt.plot(env.final_freq, color='blue', alpha=1, linewidth=2)
+plt.xlabel("Episode", fontsize=20)
+plt.ylabel("Frequency (Hz)", fontsize=20)
+plt.grid()
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.title("Reduced frequency nadir under fixed disturbance via DRL primary control", fontsize=16)
+plt.tight_layout()
+plt.savefig(save_dir + "andes_primfreq_ddpg_fix_{}.png".format(id))
+fig = plt.figure(figsize=(9, 6))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlim(left=0, right=np.max(env.t_render))
+ax.set_ylim(auto=True)
+ax.xaxis.set_tick_params(labelsize=16)
+ax.yaxis.set_tick_params(labelsize=16)
+ax.set_xlabel("Time [s]", fontsize=16)
+ax.set_ylabel("Bus Frequency [Hz]", fontsize=16)
+ax.ticklabel_format(useOffset=False)
+for i in range(env.N_Bus):
+    ax.plot(env.t_render, env.final_obs_render[:, i] * 60)
+plt.savefig("fig_primfreq_dynamics.pdf")
