169df9e0cbe37fc3fd03dfbc8c516cf9fb389026
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-04 21:51:56 -0400

    Update andes_freq_2.py

--- andes_gym/envs/andes_freq_2.py
+++ andes_gym/envs/andes_freq_2.py
@@ -110,7 +110,6 @@ class AndesPrimaryFreqControl(gym.Env):
         self.sim_case.PQ.config.q2q = 1
         self.sim_case.PQ.config.q2z = 0
         self.sim_case.PQ.config.q2i = 0
-        self.sim_case.TDS.config.critera = 0
         self.sim_case.TDS.init()
 
         # random or fixed disturbance
@@ -120,6 +119,7 @@ class AndesPrimaryFreqControl(gym.Env):
 
         # configurations
         self.sim_case.TDS.config.fixt = self.fixt
+        self.sim_case.TDS.critera = 0
 
         # sensed signals
         self.w = np.array(self.sim_case.GENROU.omega.a)
