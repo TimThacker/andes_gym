f49c5880992f4e4df5ca1aa2d154cbad38725b2e
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-05 17:19:56 -0400

    Update andes_freq_2.py

--- andes_gym/envs/andes_freq_2.py
+++ andes_gym/envs/andes_freq_2.py
@@ -176,6 +176,9 @@ class AndesPrimaryFreqControl(gym.Env):
         if np.any(self.sim_case.dae.ts.t) < 2.2:
             self.sim_case.TurbineGov.set(
                 src='uomega0', idx=self.tg_idx, value=action, attr='v')
+        else:
+            self.sim_case.TurbineGov.set(
+                src='uomega0', idx=self.tg_idx, value = 0, attr='v')
 
         # Run andes TDS to the next time and increment self.i by 1
         sim_crashed = not self.sim_to_next()
