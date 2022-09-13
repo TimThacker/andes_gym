353b1092eb8ccf7ad81f773642c3901bec34aee6
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-13 09:57:40 -0400

    Update train_prim_freq_ddpg.py

--- examples/train_prim_freq_ddpg.py
+++ examples/train_prim_freq_ddpg.py
@@ -28,8 +28,6 @@ for id in range(1, 2):
     freq.to_csv(save_dir + "andes_primfreq_ddpg_fix_{}.csv".format(id), index=False)
     freqRec = pd.DataFrame(env.best_episode_freq)
     freqRec.to_csv(save_dir + "andes_primfreq_ddpg_sim_{}.csv".format(id), index=False)
-    action_record = pd.DataFrame(env.best_action_record)
-    action_record.to_csv(save_dir + "andes_primfreq_ddpg_action_{}.csv".format(id), index=False)
     coord_record = pd.DataFrame(env.best_coord_record)
     coord_record.to_csv(save_dir + "andes_primfreq_ddpg_coord_{}.csv".format(id), index=False)
 
