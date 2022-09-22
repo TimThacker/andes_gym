d1869ef77b4e24891d9b8307486210f98c82713d
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-22 19:08:49 -0400

    Update train_prim_freq_ddpg.py

--- examples/train_prim_freq_ddpg.py
+++ examples/train_prim_freq_ddpg.py
@@ -17,12 +17,12 @@ save_dir = "C:/Users/tntth/andes_gym/examples/delay_learning_200_action_75_Prima
 for id in range(1):
     env = gym.make('AndesPrimaryFreqControl-v0')
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.2 * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.05 * np.ones(n_actions))
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64])  # kwargs == keyword arguments
     model = DDPG(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise = action_noise, learning_starts=0)
 
     time_start = time.time()
-    model.learn(total_timesteps=50000)  # we need to change the total steps with action numbers
+    model.learn(total_timesteps=10000)  # we need to change the total steps with action numbers
     
     print("training {} completed using {}".format(id, time.time() - time_start))
 
