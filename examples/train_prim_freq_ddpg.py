e28d2a664b963678a136424e4e2123f5372ef95e
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-09-23 09:13:51 -0400

    Update train_prim_freq_ddpg.py

--- examples/train_prim_freq_ddpg.py
+++ examples/train_prim_freq_ddpg.py
@@ -17,7 +17,7 @@ save_dir = "C:/Users/tntth/andes_gym/examples/delay_learning_200_action_75_Prima
 for id in range(1):
     env = gym.make('AndesPrimaryFreqControl-v0')
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.5 * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.3 * np.ones(n_actions))
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64])  # kwargs == keyword arguments
     model = DDPG(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise = action_noise, learning_starts=100)
 
