1ca4a2058b4f91abc12de13734b0c9c5f5cfba34
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2022-11-29 13:27:29 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -15,7 +15,7 @@ plot_episode = True
 save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_ls_200_test/"
 
 # Change the range size to train a larger number of models.
-for id in range(1):
+for id in range(5):
     env = gym.make('AndesPrimaryFreqControl-v0')
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))
@@ -24,7 +24,7 @@ for id in range(1):
     model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_starts=200)
 
     time_start = time.time()
-    model.learn(total_timesteps=500)  # we need to change the total steps with action numbers
+    model.learn(total_timesteps=100000)  # we need to change the total steps with action numbers
     
     print("training {} completed using {}".format(id, time.time() - time_start))
     
