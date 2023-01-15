a98927cf8ce349df03caa272af7c87c220cf8f29
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-01-15 17:08:34 -0500

    Update __init__.py

--- andes_gym/__init__.py
+++ andes_gym/__init__.py
@@ -1,6 +1,7 @@
 from gym.envs.registration import register
 from andes_gym.envs.andes_freq import AndesFreqControl
 from andes_gym.envs.andes_prim_freq import AndesPrimaryFreqControl
+from andes_gym.envs.WECC_andes_prim_freq import AndesPrimaryFreqControlWECC
 
 register(
     id='AndesFreqControl-v0',
@@ -10,4 +11,9 @@ register(
 register(
     id='AndesPrimaryFreqControl-v0',
     entry_point='andes_gym:AndesPrimaryFreqControl',
+)
+
+register(
+    id='AndesPrimaryFreqControlWECC-v0',
+    entry_point='andes_gym:AndesPrimaryFreqControlWECC',
 )
