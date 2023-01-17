fc5032ca14b840135f1e3e12b4639fdead6e9786
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-01-17 07:42:27 -0500

    Update wecc_andes_prim_freq.py

--- andes_gym/envs/wecc_andes_prim_freq.py
+++ andes_gym/envs/wecc_andes_prim_freq.py
@@ -207,7 +207,7 @@ class AndesPrimaryFreqControlWECC(gym.Env):
         # apply control for current step
         #coordsig=action*(1/100)
         
-        if self.i < 30:
+        if self.i < 50:
             coordsig=action
             #coordsig = np.zeros(self.N_Gov)
             self.sim_case.TurbineGov.set(src='uomega0', idx=self.tg_idx, value=coordsig, attr='v')
