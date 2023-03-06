80be97c204eb6340b6d08eef160b9d771202776d
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-06 15:34:51 -0500

    Update play_freq_td3.py

--- examples/play_freq_td3.py
+++ examples/play_freq_td3.py
@@ -15,25 +15,25 @@ model = TD3.load('test_primfreq_mod.pkl')
 save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_ls_200_test/"
 
 obs = env.reset()
-done = False
+    done = False
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        if done is True:
+            break
 
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-
-    if done is True:
-        env.render()
-        freqRec = pd.DataFrame(env.best_episode_freq)
-        freqRec.to_csv(save_dir + "andes_primfreq_td3_sim_{}.csv", index=False)
-        fig = plt.figure(figsize=(9, 6))
-        ax = fig.add_subplot(1, 1, 1)
-        ax.set_xlim(left=0, right=np.max(env.t_render))
-        ax.set_ylim(auto=True)
-        ax.xaxis.set_tick_params(labelsize=16)
-        ax.yaxis.set_tick_params(labelsize=16)
-        ax.set_xlabel("Time [s]", fontsize=16)
-        ax.set_ylabel("Bus Frequency [Hz]", fontsize=16)
-        ax.ticklabel_format(useOffset=False)
-        for i in range(env.N_Bus):
-            ax.plot(env.t_render, env.best_episode_freq[:, i] * 60)
-        plt.savefig(save_dir + "fig_primfreq_dynamics_best.pdf")
+env.render()
+freqRec = pd.DataFrame(env.best_episode_freq)
+freqRec.to_csv(save_dir + "andes_primfreq_td3_sim_{}.csv", index=False)
+fig = plt.figure(figsize=(9, 6))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlim(left=0, right=np.max(env.t_render))
+ax.set_ylim(auto=True)
+ax.xaxis.set_tick_params(labelsize=16)
+ax.yaxis.set_tick_params(labelsize=16)
+ax.set_xlabel("Time [s]", fontsize=16)
+ax.set_ylabel("Bus Frequency [Hz]", fontsize=16)
+ax.ticklabel_format(useOffset=False)
+for i in range(env.N_Bus):
+    ax.plot(env.t_render, env.best_episode_freq[:, i] * 60)
+plt.savefig(save_dir + "fig_primfreq_dynamics_best.pdf")
