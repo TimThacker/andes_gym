4044cb03d4206e88e537d6f061f42bbab498ec74
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-06 17:21:51 -0500

    Update play_freq_td3.py

--- examples/play_freq_td3.py
+++ examples/play_freq_td3.py
@@ -42,5 +42,5 @@ plt.savefig(save_dir + "fig_primfreq_dynamics_trained.pdf")
 sigplot = np.arange(1,30, 0.5)
 coordplot = coord_record.to_numpy()
 for i in range(env.N_Bus):
-    ax.plot(sigplot, coord_plot[:,i])
+    ax.plot(sigplot, coordplot[:,i])
 plt.savefig(save_dir + "fig_coordrecord.pdf")
