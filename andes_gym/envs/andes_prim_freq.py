fe8b46e99503d022d77634fc3d6f5077ef3610d2
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-06 13:36:37 -0500

    Update andes_prim_freq.py

--- andes_gym/envs/andes_prim_freq.py
+++ andes_gym/envs/andes_prim_freq.py
@@ -203,7 +203,7 @@ class AndesPrimaryFreqControl(gym.Env):
         
         if self.i > 2 and self.i < 20:
             coordsig=action
-            #coordsig = np.zeros(self.N_Gov)
+            coordsig = np.zeros(self.N_Gov)
             self.sim_case.TurbineGov.set(src='uomega0', idx=self.tg_idx, value=coordsig, attr='v')
             self.coord_record.append(coordsig)
         else:
