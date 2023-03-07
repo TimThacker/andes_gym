3753968c74a04e516149155bf2d646ac2e19c817
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-07 14:22:16 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -15,17 +15,17 @@ plot_episode = True
 save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_ls_200_test/"
 
 # Change the range size to train a larger number of models.
-for id in range(1):
+for id in range(5):
     env = gym.make('AndesPrimaryFreqControl-v0')
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     train_freq = (1,"episode")
-    policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[256, 128, 64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, target_policy_noise=0.1, train_freq=train_freq, learning_rate = 0.0001, learning_starts=200, tensorboard_log="./td3_tensorboard/")
+    policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128,64])  # kwargs == keyword arguments
+    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_rate = 0.0001, learning_starts=100*id, tensorboard_log="./td3_tensorboard/")
     #model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, train_freq=train_freq, learning_starts=200)
 
     time_start = time.time()
-    model.learn(total_timesteps=50000,tb_log_name="first_run")  # we need to change the total steps with action numbers
+    model.learn(total_timesteps=50000,tb_log_name="TD3_test_{}".fomrat(id))  # we need to change the total steps with action numbers
     
     print("training {} completed using {}".format(id, time.time() - time_start))
     
