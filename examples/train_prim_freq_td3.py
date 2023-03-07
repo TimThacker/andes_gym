d729e21865a49f92ea7040727a211e57b33d676b
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-07 16:36:38 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -21,9 +21,8 @@ for id in range(1):
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     train_freq = (1,"episode")
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128,64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_starts=200, tensorboard_log="./td3_tensorboard/")
-    #model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, train_freq=train_freq, learning_starts=200)
-
+    #model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_starts=200, tensorboard_log="./td3_tensorboard/")
+    model= TD3.load('model_td3_test2.pkl')
     time_start = time.time()
     model.learn(total_timesteps=50000,tb_log_name="TD3_test")  # we need to change the total steps with action numbers
     
