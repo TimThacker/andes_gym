0771d4bf2b7a45a2c203cbec6578b1d3a9de91bc
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-06 23:01:03 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -20,8 +20,8 @@ for id in range(1):
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.001 * np.ones(n_actions))
     train_freq = (1,"episode")
-    policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size = 400 learning_starts=200)
+    policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64, 64])  # kwargs == keyword arguments
+    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size = 400, learning_starts=200)
     #model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, train_freq=train_freq, learning_starts=200)
 
     time_start = time.time()
