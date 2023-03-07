8360a2e635cbc98e2579bf07158922a2564a897d
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-06 19:11:32 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -18,10 +18,10 @@ save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_ls_200_test/"
 for id in range(1):
     env = gym.make('AndesPrimaryFreqControl-v0')
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     train_freq = (1,"episode")
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64, 64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_starts=400)
+    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, learning_starts=200)
 
     time_start = time.time()
     model.learn(total_timesteps=50000)  # we need to change the total steps with action numbers
