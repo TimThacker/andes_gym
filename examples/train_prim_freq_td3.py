986c059ee62adbb3231459cf378d1730c58c70e3
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-08 17:37:02 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -32,7 +32,7 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
         super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
         self.check_freq = check_freq
         self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, "best_model_{}".format(id), index=False)
+        self.save_path = os.path.join(log_dir, "best_model_{}".format(id))
         self.best_mean_reward = -np.inf
 
     def _init_callback(self) -> None:
