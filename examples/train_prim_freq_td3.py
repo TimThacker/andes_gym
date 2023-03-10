d79a163c6963fcc284a20facbef44dc106d36fd6
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-10 08:38:21 -0500

    Update train_prim_freq_td3.py

--- examples/train_prim_freq_td3.py
+++ examples/train_prim_freq_td3.py
@@ -67,7 +67,7 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 plot_episode = True
-save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_batchsize_test/"
+save_dir = "C:/Users/tntth/andes_gym/examples/TD3_data_ls_test/"
 log_dir = "tmp/"
 os.makedirs(log_dir, exist_ok=True)
 
@@ -79,10 +79,10 @@ for id in range(5):
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     train_freq = (1,"episode")
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128,64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size = id*100+100, learning_starts=200, buffer_size=2000000, tensorboard_log="./td3_tensorboard_batchsize/")
+    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size = 100 learning_starts=id*100, tensorboard_log="./td3_tensorboard_ls/")
     callback = SaveOnBestTrainingRewardCallback(id, check_freq=300, log_dir=log_dir)
     time_start = time.time()
-    model.learn(total_timesteps=100000,tb_log_name="TD3_test_batchsize", callback=callback)  # we need to change the total steps with action numbers
+    model.learn(total_timesteps=100000,tb_log_name="TD3_test_ls", callback=callback)  # we need to change the total steps with action numbers
     
     print("training {} completed using {}".format(id, time.time() - time_start))
     
