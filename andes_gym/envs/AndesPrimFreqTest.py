b4e8fda4253ef9dcd163b62b8414317971094ca6
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-16 14:20:34 -0400

    Update AndesPrimFreqTest.py

--- andes_gym/envs/AndesPrimFreqTest.py
+++ andes_gym/envs/AndesPrimFreqTest.py
@@ -107,7 +107,7 @@ class AndesPrimaryFreqControlTest(gym.Env):
         self.best_episode_freq = []
         self.coord_record = []
         self.best_coord_record = []
-        self.rocof_window = []
+        self.rocof_window = [1]
 
         
     
