bffbd046a70a116b44580f472ccb3002def6b20e
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-16 14:07:52 -0400

    Update AndesPrimFreqTest.py

--- andes_gym/envs/AndesPrimFreqTest.py
+++ andes_gym/envs/AndesPrimFreqTest.py
@@ -195,10 +195,10 @@ class AndesPrimaryFreqControlTest(gym.Env):
 
         # apply control for current step
         #coordsig=action*(1/100)
-        if self.i < 2:
-            self.sim_case.dae.ts.unpack()
-            windowdata = np.array(self.sim_case.dae.y[self.dwdt]).reshape((-1, ))
-            self.rocof_window = np.append(self.rocof_window,windowdata)
+        #if self.i < 2:
+        self.sim_case.dae.ts.unpack()
+        windowdata = np.array(self.sim_case.dae.y[self.dwdt]).reshape((-1, ))
+        self.rocof_window = np.append(self.rocof_window,windowdata)
         
         if self.i > 2 and self.i < 20:
             coordsig=action
