45bbce5accb2ddb20c4d23d87c2073c3bc07e9ec
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-16 13:43:14 -0400

    Update AndesPrimFreqTest.py

--- andes_gym/envs/AndesPrimFreqTest.py
+++ andes_gym/envs/AndesPrimFreqTest.py
@@ -225,8 +225,7 @@ class AndesPrimaryFreqControlTest(gym.Env):
             reward -= 9999
             done = True
 
-        if self.i < 2:
-            self.rocof_window = np.append(rocof)
+        self.rocof_window = np.append(rocof)
       
         if self.i > 2 and not sim_crashed and done:
             #reward -= np.sum(np.abs(30000 * rocof ))  # the final episode
