71ed989eb78513f3b175748ffe0f72ab50178e1a
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-24 17:02:47 -0400

    Update play_freq_td3.py

--- examples/play_freq_td3.py
+++ examples/play_freq_td3.py
@@ -9,10 +9,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-env = gym.make("AndesPrimaryFreqControl-v0")
+env = gym.make("AndesPrimaryFreqControlTest-v0")
 # policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128, 64])
 #model = TD3.load('model_actnoise_0001_test.pkl')
-model = TD3.load('IEEE14_Single_Dist.zip')
+model = TD3.load('best_model_0.zip')
 save_dir = "C:/Users/tntth/andes_gym/examples/Trained_Model_Application/"
 
 obs = env.reset()
@@ -26,17 +26,17 @@ while True:
        
 env.render()
 rocof_record = pd.DataFrame(env.best_episode_rocof)
-rocof_record.to_csv(save_dir + "andes_primfreq_td3_rocof_singledist_040_over.csv", index=False)
+rocof_record.to_csv(save_dir + "andes_primfreq_td3_rocof_multidist_030.csv", index=False)
 #norm_rocof_record = pd.DataFrame(env.best_episode_rocof_norm)
 #norm_rocof_record.to_csv(save_dir + "andes_primfreq_td3_norm_rocof_dist_.csv", index=False)
 #rocof_window = pd.DataFrame(env.rocof_window)
 #rocof_window.to_csv(save_dir + "andes_primfreq_td3_rocof_window_.csv", index=False)
 coord_record = pd.DataFrame(env.best_coord_record)
-coord_record.to_csv(save_dir + "andes_primfreq_td3_coord_singledist_040_over.csv", index=False)
+coord_record.to_csv(save_dir + "andes_primfreq_td3_coord_multidist_030.csv", index=False)
 freqRec = pd.DataFrame(env.best_episode_freq)
-freqRec.to_csv(save_dir + "andes_primfreq_td3_sim_singledist_040_over.csv", index=False)
+freqRec.to_csv(save_dir + "andes_primfreq_td3_sim_multidist_030.csv", index=False)
 gov_tm = pd.DataFrame(env.best_episode_govdata)
-gov_tm.to_csv(save_dir + "andes_primfreq_td3_govdata_singledist_040_over.csv", index=False)
+gov_tm.to_csv(save_dir + "andes_primfreq_td3_govdata_multidist_030.csv", index=False)
 #totalRewards = pd.DataFrame(env.episode_reward)
 #totalRewards.to_csv(save_dir + "andes_primfreq_td3_episodeRewards.csv", index=False)
 fig = plt.figure(figsize=(9, 6))
@@ -50,4 +50,4 @@ ax.set_ylabel("Bus Frequency [Hz]", fontsize=16)
 ax.ticklabel_format(useOffset=False)
 for i in range(env.N_Bus):
     ax.plot(env.t_render, env.best_episode_freq[:, i] * 60)
-plt.savefig(save_dir + "fig_primfreq_dynamics_singledist_040_over.pdf")
+plt.savefig(save_dir + "fig_primfreq_dynamics_multidist_030.pdf")
