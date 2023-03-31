93a4691aa64e3524226d4c7200c57bc7bd2f51cc
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-03-31 10:01:13 -0400

    Update wecc_train_prim_freq_td3.py

--- examples/wecc_train_prim_freq_td3.py
+++ examples/wecc_train_prim_freq_td3.py
@@ -77,11 +77,11 @@ for id in range(15):
     env = gym.make('AndesPrimaryFreqControlWECC-v0')
     env = Monitor(env, log_dir)
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     train_freq = (1,"episode")
     policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=[128,64])  # kwargs == keyword arguments
-    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size=200, learning_starts=200, tensorboard_log="./td3_tensorboard_WECC/")
-    callback = SaveOnBestTrainingRewardCallback(id, check_freq=300, log_dir=log_dir)
+    model = TD3(MlpPolicy, env, verbose=1, policy_kwargs=policy_kwargs, action_noise=action_noise, train_freq=train_freq, batch_size=400, learning_starts=400, tensorboard_log="./td3_tensorboard_WECC/")
+    callback = SaveOnBestTrainingRewardCallback(id, check_freq=600, log_dir=log_dir)
     time_start = time.time()
     model.learn(total_timesteps=100000,tb_log_name="TD3_WECC", callback=callback)  # we need to change the total steps with action numbers
     
