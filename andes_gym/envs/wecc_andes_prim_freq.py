046e2e6f329a0c1f98dc58950e96e349033fa294
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-04-03 11:13:28 -0400

    Update wecc_andes_prim_freq.py

--- andes_gym/envs/wecc_andes_prim_freq.py
+++ andes_gym/envs/wecc_andes_prim_freq.py
@@ -72,8 +72,8 @@ class AndesPrimaryFreqControlWECC(gym.Env):
         self.N_obs = 6   # 3 areas, COIfreq, COIrocof
         self.N_coi = 3
 
-        self.action_space = spaces.Box(low=-.002, high=.02, shape=(self.N_coi,))
-        self.observation_space = spaces.Box(low=-0.2, high=0.2, shape=(2*self.N_coi,))
+        self.action_space = spaces.Box(low=-.01, high=.03, shape=(self.N_coi,))
+        self.observation_space = spaces.Box(low=-0.3, high=0.3, shape=(2*self.N_coi,))
 
         # This code is executed by the index of the action applications, rather than
         # the time domain simulation time step from ANDES.
