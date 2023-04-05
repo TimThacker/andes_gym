c94e1b2173ad1e10b1de194386adf249e79db147
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2023-04-05 00:22:59 -0400

    Update wecc_andes_prim_freq.py

--- andes_gym/envs/wecc_andes_prim_freq.py
+++ andes_gym/envs/wecc_andes_prim_freq.py
@@ -136,6 +136,7 @@ class AndesPrimaryFreqControlWECC(gym.Env):
         self.sim_case.PQ.config.q2q = 1
         self.sim_case.PQ.config.q2z = 0
         self.sim_case.PQ.config.q2i = 0
+        self.sim_case.PQ.pq2z = 0
         self.sim_case.TDS.init()
         self.sim_case.TDS.config.criteria = 0
 
